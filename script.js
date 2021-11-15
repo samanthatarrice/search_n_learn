5cb11da2135f3c45499cde0670dc333a762347ea
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-15 00:54:24 -0300

    Add video to calendar

--- script.js
+++ script.js
@@ -3,5 +3,38 @@ const imagesToFind = Array.from(document.querySelectorAll('.images-to-find'));
 imagesToFind.forEach(element => {
   element.addEventListener('click', () => {
     element.style.display = 'none';
+
+    if (element.className.includes('calendar')) {
+
+      const modalBackground = document.querySelector('.modal-background');
+      modalBackground.style.display = 'block';
+
+      const twelveDays = document.querySelector('.twelve-days');
+      twelveDays.style.display = 'block';
+      // const modalBackground = document.createElement('div');
+      // document.body.appendChild(modalBackground).classList.add('modal-background');
+
+      // const modalContent = document.createElement('div');
+      // modalBackground.appendChild(modalContent).classList.add('modal-content');
+
+      const close = document.createElement('img');
+      close.src = 'images/close.png';
+      modalBackground.appendChild(close).classList.add('close');
+
+      close.addEventListener('click', () => modalBackground.style.display = 'none');
+    }
+
   })
 })
+
+// `
+//         <iframe width="80%" height="auto"
+//         src="https://youtu.be/QYyhDvuq8_Y?controls=0">
+//         </iframe>
+//         `
+      // const twelveDaysVid = document.createElement('iframe');
+      // twelveDaysVid.src = 'https://youtu.be/QYyhDvuq8_Y?controls=0';
+      // modalContent.appendChild(twelveDaysVid);
+
+
+      // <iframe class="twelve-days" width="420" height="315" src="https://youtu.be/QYyhDvuq8_Y?controls=0"></iframe>
